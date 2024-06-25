b0b0c92bb8dd6941e1ea7b779a015aac2e130c5d
Blake Pat <blakepat@me.com>
2024-06-25 19:32:29 -0400

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Blake Patenaude on 2024-06-25.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
