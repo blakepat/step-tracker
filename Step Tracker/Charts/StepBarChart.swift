acf2771445f376ae55fae318e54223714bba1778
Blake Patenaude <blakepatenaude@MacBookPro.phub.net.cable.rogers.com>
2024-11-07 16:15:17 -0500

    Added Weight Line Chart

--- Step Tracker/Charts/StepBarChart.swift
+++ Step Tracker/Charts/StepBarChart.swift
@@ -114,5 +114,5 @@ struct StepBarChart: View {
 }
 
 #Preview {
-    StepBarChart(selectedStat: .steps, chartData: HealthMetric.mockData)
+    StepBarChart(selectedStat: .steps, chartData: MockData.steps)
 }
---
+++ Step Tracker/Charts/WeightLineChart.swift
@@ -0,0 +1,54 @@
+//
+//  WeightLineChart.swift
+//  Step Tracker
+//
+//  Created by Blake Patenaude on 2024-11-07.
+//
+
+import SwiftUI
+import Charts
+
+struct WeightLineChart: View {
+    
+    var selectedStat: HealthMetricContext
+    var chartData: [HealthMetric]
+    
+    var body: some View {
+        VStack {
+            NavigationLink(value: selectedStat) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Label("Weight", systemImage: "figure")
+                            .font(.title3.bold())
+                            .foregroundStyle(.indigo)
+                        
+                        Text("Avg: 180lbs")
+                            .font(.caption)
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                }
+            }
+            .foregroundStyle(.secondary)
+            .padding(.bottom, 12)
+            
+            Chart {
+                ForEach(chartData) { weight in
+                    AreaMark(x: .value("Day", weight.date), y: .value("Weight", weight.value))
+                        .foregroundStyle(Gradient(colors: [.blue.opacity(0.5), .clear]))
+                    
+                    LineMark(x: .value("Day", weight.date), y: .value("Weight", weight.value))
+                }
+            }
+            .frame(height: 150)
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+    }
+}
+
+#Preview {
+    WeightLineChart(selectedStat: .weight, chartData: MockData.weights)
+}
